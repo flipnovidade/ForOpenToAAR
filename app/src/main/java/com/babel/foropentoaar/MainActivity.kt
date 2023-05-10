f0435847e96f702542a7b001dd35c613dfdb23cd
Felippe Ferreira <felippe.ferreira@bbnk.com.br>
2023-05-10 22:11:17 +0100

    tentando rodar projeto com gradle editado

--- app/src/main/java/com/babel/foropentoaar/MainActivity.kt
+++ app/src/main/java/com/babel/foropentoaar/MainActivity.kt
@@ -6,12 +6,15 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.babel.foropentoaar.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
-import com.timwe.tti2sdk.ui.splash.SplashActivity
-import com.timwe.init.ScreenCallback
-import com.timwe.init.Tti2
-import com.timwe.init.Tti2Request
-import com.timwe.init.UTM
-import com.timwe.init.UserProfile
+//import com.timwe.tti2sdk.ui.splash.SplashActivity
+//import com.timwe.init.ScreenCallback
+//import com.timwe.init.Tti2
+//import com.timwe.init.Tti2Request
+//import com.timwe.init.UTM
+//import com.timwe.init.UserProfile
+//import com.timwe.init.*
+//import com.timwe.tti2sdk.ui.splash.SplashActivity
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,22 +33,22 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Call SDK", Snackbar.LENGTH_LONG).setAction("Action", null).show()
 
             try {
-                val listPlans = listOf<String>("prepaid", "postpaid")
-                val listTier = listOf<String>("gold", "silver", "platinum", "diamond")
-                val listLang = listOf<String>("en", "idn")
-
-                val userProfile =  UserProfile()
-
-                userProfile.userMsisdn = "62458000010"
-                userProfile.email = "email@email.com"
-                userProfile.lang = listLang[0]
-                userProfile.tier = listTier[0]
-                userProfile.plan = listPlans[0]
-
-                val intent = Intent(this@MainActivity, SplashActivity::class.java)
-                intent.putExtra("USER_PROFILE_KEY", userProfile)
-                intent.putExtra("IS_DEBUGGABLE", true)
-                startActivity(intent)
+//                val listPlans = listOf<String>("prepaid", "postpaid")
+//                val listTier = listOf<String>("gold", "silver", "platinum", "diamond")
+//                val listLang = listOf<String>("en", "idn")
+//
+//                val userProfile =  UserProfile()
+//
+//                userProfile.userMsisdn = "62458000010"
+//                userProfile.email = "email@email.com"
+//                userProfile.lang = listLang[0]
+//                userProfile.tier = listTier[0]
+//                userProfile.plan = listPlans[0]
+//
+//                val intent = Intent(this@MainActivity, SplashActivity::class.java)
+//                intent.putExtra("USER_PROFILE_KEY", userProfile)
+//                intent.putExtra("IS_DEBUGGABLE", true)
+//                startActivity(intent)
 
             }catch (r: java.lang.Exception){
                 r.printStackTrace()
